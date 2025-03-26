534e541dedc5c2a0a2658ff878768daf48e5a8ea
Cortland Miller <cortlandm99@gmail.com>
2025-03-26 17:43:47 -0500

    Created a Customer controller with an index and show view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "customer/index"
+  get "customer/show"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
