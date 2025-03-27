b47de2a507bfe80abdfcff0aa9d83d133cbf1864
Cortland Miller <cortlandm99@gmail.com>
2025-03-27 16:19:09 -0500

    Added actions 'alphabetized' and 'missing_email' to Customer controller and updated it so that it met requirements.

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -1,7 +1,13 @@
 class CustomerController < ApplicationController
   def index
+    @customers = Customer.all
   end
 
-  def show
+  def alphabetized
+    @customers = Customer.order(:name)
+  end
+
+  def missing_email
+    @customers = Customer.where(email: [ nil, "" ])
   end
 end
